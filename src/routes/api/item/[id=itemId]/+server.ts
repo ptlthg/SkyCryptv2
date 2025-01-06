20e66357f142c556ede4b3cd8181bf07a48ebf3c
DuckySoLucky <robertkovac160@gmail.com>
2025-01-06 15:44:34 +0100

    fix(/item): use fetch()

--- src/routes/api/item/[id=itemId]/+server.ts
+++ src/routes/api/item/[id=itemId]/+server.ts
@@ -2,14 +2,27 @@ import { renderItem } from "$lib/server/helper/renderer";
 import { error } from "@sveltejs/kit";
 import type { RequestHandler } from "./$types";
 
-// GET /api/head/[id=itemId]
-export const GET: RequestHandler = async ({ params, cookies }) => {
+export const GET: RequestHandler = async ({ params, cookies, fetch }) => {
   const { id } = params;
 
   try {
     const [newId, damage] = id.split(":");
     const packs = JSON.parse(cookies.get("disabledPacks") || "[]");
     const attachment = await renderItem(newId, { damage: isNaN(parseInt(damage)) ? 0 : parseInt(damage), packs });
+
+    if (typeof attachment.image === "string" && attachment.image.startsWith("/api/")) {
+      const imageResponse = await fetch(attachment.image);
+      if (!imageResponse.ok) {
+        throw new Error("Failed to fetch image from API");
+      }
+
+      const imageBuffer = await imageResponse.arrayBuffer();
+      return new Response(imageBuffer, {
+        headers: {
+          "Content-Type": "image/png"
+        }
+      });
+    }
 
     return new Response(attachment.image, {
       headers: {
