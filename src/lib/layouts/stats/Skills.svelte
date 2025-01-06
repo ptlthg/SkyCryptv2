3de23af60689da9de26437e65a5649504c6e0f89
DuckySoLucky <robertkovac160@gmail.com>
2025-01-06 17:55:44 +0100

    fix(skillbar): svelte 5

--- src/lib/layouts/stats/Skills.svelte
+++ src/lib/layouts/stats/Skills.svelte
@@ -12,7 +12,7 @@
 
   <div class="flex flex-col flex-wrap gap-x-4 gap-y-2 @md:flex-row">
     {#each Object.entries(profile.skills.skills) as [skillName, skillData]}
-      <Skillbar skill={skillName} {skillData} APIEnabled={profile.apiSettings.skills} />
+      <Skillbar skill={skillName} {skillData} apiEnabled={profile.apiSettings.skills} />
     {/each}
   </div>
 </div>
