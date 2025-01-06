d5f3825da432f8e41ccf3e091aca9e1258694546
ptlthg <24925519+ptlthg@users.noreply.github.com>
2025-01-06 16:43:26 -0500

    Stop using afterNavigate for url rewriting

--- src/routes/stats/[ign]/[[profile]]/+page.svelte
+++ src/routes/stats/[ign]/[[profile]]/+page.svelte
@@ -1,34 +1,36 @@
 <script lang="ts">
-  import { afterNavigate, goto } from "$app/navigation";
+  import { goto } from "$app/navigation";
   import { page } from "$app/state";
   import { setProfileCtx } from "$ctx/profile.svelte";
   import Main from "$lib/layouts/stats/Main.svelte";
   import type { ValidStats } from "$types/stats";
+  import { untrack } from "svelte";
   import type { PageData } from "./$types";
 
   let { data }: { data: PageData } = $props();
 
-  // Update the URL to include the profile if it's missing
-  afterNavigate(async () => {
-    if (!data.user) return;
-
-    const { username, profile_cute_name } = data.user;
-    if (!username) return;
-
-    const current = page.url.pathname;
-    const wanted = `/stats/${username}/${profile_cute_name || ""}`;
-
-    if (current !== wanted) {
-      const newUrl = page.url.toString().replace(current, wanted);
-      goto(newUrl, { replaceState: true });
-    }
-  });
-
   // Initialize the profile context
   setProfileCtx(data.user as unknown as ValidStats);
+
   // Update the profile context when the data changes
   $effect(() => {
     setProfileCtx(data.user as unknown as ValidStats);
+
+    untrack(() => {
+      if (!data.user) return;
+
+      const { username, profile_cute_name } = data.user;
+      if (!username) return;
+
+      const current = page.url.pathname;
+      const wanted = `/stats/${username}/${profile_cute_name || ""}`;
+
+      // Update the URL to match the username and cute name
+      if (current !== wanted) {
+        const newUrl = page.url.toString().replace(current, wanted);
+        goto(newUrl, { replaceState: true });
+      }
+    });
   });
 </script>
 
