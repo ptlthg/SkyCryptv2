617d6bca3c57a9558cf5a431a022a9a6e94876ef
DarthGigi <47110839+DarthGigi@users.noreply.github.com>
2025-01-11 16:47:45 +0100

    feat(CollapsibleSection): add subtitle support for collapsible sections in Dungeons

--- src/lib/components/CollapsibleSection.svelte
+++ src/lib/components/CollapsibleSection.svelte
@@ -7,7 +7,7 @@
   import type { Snippet } from "svelte";
   import { slide } from "svelte/transition";
 
-  let { id, class: className, children }: { id: string; class?: string; children?: Snippet } = $props();
+  let { id, class: className, children, subtitle }: { id: string; class?: string; children?: Snippet; subtitle?: Snippet } = $props();
 
   let transormedID = $derived(id.replaceAll(" ", "_"));
 </script>
@@ -15,7 +15,11 @@
 <Collapsible.Root asChild let:builder bind:open={() => $collapsePreferences[transormedID.toLowerCase()] ?? true, (v) => ($collapsePreferences[transormedID.toLowerCase()] = v)}>
   <section id={transormedID} use:builder.action {...builder} class={cn("scroll-m-32", className)}>
     <Collapsible.Trigger class="flex items-center justify-between">
-      <SectionTitle>{id}</SectionTitle>
+      {#if !subtitle}
+        <SectionTitle>{id}</SectionTitle>
+      {:else}
+        {@render subtitle()}
+      {/if}
       <ChevronDown class={cn("h-6 w-6 text-text/60 transition-all duration-300", { "rotate-180": $collapsePreferences[transormedID.toLowerCase()] ?? true })} />
     </Collapsible.Trigger>
     <Collapsible.Content transition={slide}>
