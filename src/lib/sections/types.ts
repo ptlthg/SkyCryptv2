03725c2106575b1587f3bc0d0d9806d0f3a38974
DarthGigi <47110839+DarthGigi@users.noreply.github.com>
2025-01-12 14:38:35 +0100

    experiment(Sections): replace lazy loading with eager loading for section components

--- src/lib/sections/types.ts
+++ src/lib/sections/types.ts
@@ -3,4 +3,6 @@ import type { Component } from "svelte";
 export type SectionName = "Armor" | "Weapons" | "Accessories" | "Pets" | "Inventory" | "Skills" | "Dungeons" | "Slayer" | "Minions" | "Bestiary" | "Collections" | "Crimson_Isle" | "Rift" | "Misc";
 
 export type SectionComponents = Record<SectionName, Promise<{ default: Component }> | null>;
+export type SectionComponentsEager = Record<SectionName, Component | null>;
+
 export type SectionID = { id: number; name: SectionName };
